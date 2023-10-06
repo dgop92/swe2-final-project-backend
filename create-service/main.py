0e55fd5afecaa80d8407402932b6803b42b88436
dgop92 <inevaup@gmail.com>
2023-10-06 17:03:45 -0500

    feat: add create service repository

--- create-service/main.py
+++ create-service/main.py
@@ -1,25 +1,37 @@
 import logging
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
+from config.database import get_mongo_database
 from config.logging import config_logger
 from core.entities import People
+from core.repository import PeopleMongoRepository, RepositoryException
 
 config_logger()
 logger = logging.getLogger(__name__)
 
 app = FastAPI(
-    title="People API",
-    description="API to manage people",
+    title="People Create Service",
+    description="API to create people for the people service",
     contact={
         "name": "dgop92",
         "url": "https://github.com/dgop92",
     },
 )
+monog_database = get_mongo_database()
+repository = PeopleMongoRepository(monog_database)
 
 
 @app.post("/create")
 def create(people: People):
+    try:
+        repository.create(people)
+    except RepositoryException as e:
+        logger.error(e)
+        raise HTTPException(
+            status_code=400,
+            detail=str(e),
+        )
     return people
 
 
