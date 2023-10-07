365b2eef77be293f4fc5015f01c9613ebed4a1d0
dgop92 <inevaup@gmail.com>
2023-10-07 15:38:32 -0500

    feat: add update - delete rest implementations

--- api-gateway/core/people_service/rest_people_service.py
+++ api-gateway/core/people_service/rest_people_service.py
@@ -31,7 +31,20 @@ class RestPeopleService(PeopleService):
             raise HTTPException(status_code=500)
 
     def update(self, id: str, body: PeopleBody) -> PeopleResponse:
-        return {"id": id, "name": "John Doe", "age": 30}
+        r = requests.patch(
+            f"{PEOPLE_SERVICE_URLS['update']}/update/{id}", json=body, timeout=15
+        )
+
+        if r.status_code == 200:
+            return r.json()
+        elif r.status_code == 422:
+            raise HTTPException(status_code=422, detail=r.json()["detail"])
+        elif r.status_code == 400:
+            raise HTTPException(status_code=400, detail=r.json()["detail"])
+        elif 404:
+            raise HTTPException(status_code=404, detail=r.json()["detail"])
+        else:
+            raise HTTPException(status_code=500)
 
     def detail(self, id: str) -> PeopleResponse:
         r = requests.get(f"{PEOPLE_SERVICE_URLS['read']}/detail/{id}", timeout=15)
@@ -44,7 +57,14 @@ class RestPeopleService(PeopleService):
             raise HTTPException(status_code=500)
 
     def delete(self, id: str) -> PeopleResponse:
-        return {}
+        r = requests.delete(f"{PEOPLE_SERVICE_URLS['delete']}/delete/{id}", timeout=15)
+
+        if r.status_code == 204:
+            return {}
+        elif r.status_code == 404:
+            raise HTTPException(status_code=404, detail=r.json()["detail"])
+        else:
+            raise HTTPException(status_code=500)
 
     def list(self, query: PeopleQuery) -> List[PeopleResponse]:
         return [{"id": "1", "name": "John Doe", "age": 30}]
